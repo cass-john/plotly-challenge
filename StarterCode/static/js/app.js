c563f06ca12dca391f8a3ba2af0907de911d9a73
John Cass <cass.john@icloud.com>
2021-07-28 12:40:00 -0500

    added names to drop down

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,23 @@
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
 // https://nu.bootcampcontent.com/NU-Coding-Bootcamp/nu-chi-data-pt-02-2021-u-c/-/blob/master/01-Lesson-Plans/15-Interactive-Visualizations-and-Dashboards/3/Activities/08-Stu_Github_Pages/Solved/plot.js
-d3.json("data/samples.json").then((incomingData) => {
-    console.log(incomingdata)
+// Initializes the page with a default plot
+function init() {
 
+    // Grabbing the dropdown element
+    let selector = d3.select('#selDataset');
 
+d3.json("data/samples.json").then((sampleData) => {
+    console.log(sampledata)
+    let namesdata = sampleData.names;
+
+    // inserting names into drop down
+    selector.selectAll('option')
+    .data(names)
+    .enter()
+    .append('option')
+    .attr('value', d => d)
+    .text(d => d);
 
 
 }
