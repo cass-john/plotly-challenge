15c4d580da56ad85536bdf5b01e644809a2936ed
John Cass <cass.john@icloud.com>
2021-07-28 12:11:26 -0500

    fetched data from json

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,10 @@
+// Use d3.json() to fetch data from JSON file
+// Incoming data is internally referred to as incomingData
+// https://nu.bootcampcontent.com/NU-Coding-Bootcamp/nu-chi-data-pt-02-2021-u-c/-/blob/master/01-Lesson-Plans/15-Interactive-Visualizations-and-Dashboards/3/Activities/08-Stu_Github_Pages/Solved/plot.js
+d3.json("data/samples.json").then((incomingData) => {
+    console.log(incomingdata)
+
+
+
+
+}
