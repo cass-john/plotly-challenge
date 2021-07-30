d95bb607cd2a80be66f4d28feeb31ce0f56a53ad
John Cass <cass.john@icloud.com>
2021-07-29 19:25:29 -0500

    grabbed dropdown element and built plot with data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,23 +1,16 @@
-// Use d3.json() to fetch data from JSON file
-// Incoming data is internally referred to as incomingData
-// https://nu.bootcampcontent.com/NU-Coding-Bootcamp/nu-chi-data-pt-02-2021-u-c/-/blob/master/01-Lesson-Plans/15-Interactive-Visualizations-and-Dashboards/3/Activities/08-Stu_Github_Pages/Solved/plot.js
-// Initializes the page with a default plot
-function init() {
+function optionChanged(){
 
     // Grabbing the dropdown element
-    let selector = d3.select('#selDataset');
+    let bio = d3.select("#selDataset").node().value;
 
-d3.json("data/samples.json").then((sampleData) => {
-    console.log(sampledata)
-    let namesdata = sampleData.names;
+    //build plot with new data
+    buildPlot(bio);
+};
 
-    // inserting names into drop down
-    selector.selectAll('option')
-    .data(names)
-    .enter()
-    .append('option')
-    .attr('value', d => d)
-    .text(d => d);
-
-
-}
+function buildPlot(bio){
+    d3.json("././samples.json").then(function(data){
+        let filteredData = data.samples.filter(d => d.id === bio);
+        let sample_values = filteredData[0].sample_values.sort(function(a,b){return b-a});
+        let otu_ids = filteredData[0].otu_ids;
+        let otu_labels = filteredData[0].otu_labels;  
+};
